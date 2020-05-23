528496096896803a0530818b78b9eea1b7462cd0
Dixing Xu <dixingxu@gmail.com>
2020-05-23 12:31:56 +0800

    first commit.

---
+++ index.js
@@ -0,0 +1,10 @@
+/*
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+'use strict';
+
+const ChianMedic = require('./lib/chainmedic');
+
+module.exports.ChainMedic = ChainMedic;
+module.exports.contracts = [ ChainMeidc ];
